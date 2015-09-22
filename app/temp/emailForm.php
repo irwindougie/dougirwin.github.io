c074ab671a2e7afae1cccdd8185732a1099aa493
Doug Irwin <doug.irwin@gogana.net>
2015-09-22 13:15:32 -0600

    test

---
+++ app/temp/emailForm.php
@@ -0,0 +1,31 @@
+﻿  <form method="post" action="emailForm.php">
+      <label>Name:</label>
+      <input required name="sender" />
+
+      <label>Email address:</label>
+      <input name="senderEmail" />
+
+      <label>Message:</label>
+      <textarea rows="5" cols="20" name="message"></textarea>
+
+      <input type="submit" name="submit" />
+  </form>
+
+  <?php
+
+if($_POST["submit"]) {
+    $recipient="contact@dougirw.in";
+    $subject="Form to email message";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>
+    ");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+    }
+
+    ?>
